a55985d7f7698a9d1c5e07058576b95b1915bec5
ExgleGaming <EaglesFan91x@outlook.com>
2025-12-10 22:52:42 -0500

    state machine 1

---
+++ scripts/entities/player/states/base_state.gd
@@ -0,0 +1,26 @@
+class_name BaseState
+extends Node
+
+
+var parent_state_machine: StateMachine
+
+
+@warning_ignore("unused_parameter")
+func update_physics(delta: float) -> void:
+    pass
+
+
+func update() -> void:
+    pass
+
+
+func enter() -> void:
+    pass
+
+
+func exit() -> void:
+    pass
+
+
+func ready() -> void:
+    pass
